2ac07573dc77a4f61223c17739c327a5d8622146
Joseph Record <joseph.record@gmail.com>
2018-12-03 16:02:04 -0500

    Update Aoc2018_day2.py

--- Aoc2018_day2.py
+++ Aoc2018_day2.py
@@ -1,31 +1,44 @@
 from collections import Counter
 
-count2 = 0
-count3 = 0
+# count2 = 0
+# count3 = 0
 
-with open("C:\\Users\\jrecord\\Documents\\GitHub\\AoC2018\\AoC2018_day2.txt") as f:
-        line = f.readline()
-        print(line)
+# with open("C:\\Users\\jrecord\\Documents\\GitHub\\AoC2018\\AoC2018_day2.txt") as f:
+#         line = f.readline()
+#         print(line)
 
-        c = dict(Counter(line))
+#         c = dict(Counter(line))
         
-        if 2 in c.values():
-            count2 += 1
-        if 3 in c.values():
-            count3 += 1
+#         if 2 in c.values():
+#             count2 += 1
+#         if 3 in c.values():
+#             count3 += 1
 
-        cnt = 1
-
-
-        while line:
-            line = f.readline()
-            c = dict(Counter(line))
-            if 2 in c.values():
-                count2 += 1
-            if 3 in c.values():
-                count3 += 1
-            cnt += 1
-
-print(count2*count3)
+#         cnt = 1
 
 
+#         while line:
+#             line = f.readline()
+#             c = dict(Counter(line))
+#             if 2 in c.values():
+#                 count2 += 1
+#             if 3 in c.values():
+#                 count3 += 1
+#             cnt += 1
+
+# print(count2*count3)
+
+
+# if (c - (c & d)) == 1 and (d - (c & d)) == 1
+
+l = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
+
+for i in range(len(l)):
+    for j in range(len(l)):
+        c = Counter(l[i])
+        d = Counter(l[j])
+
+        if len(c - d) == 1 and len(d - c) == 1:
+            print(c)
+            print(d)
+            break
