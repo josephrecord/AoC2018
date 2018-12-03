cfec700f72d4f81931f9cbaa2a6f9d3d6df0951d
Joseph Record <joseph.record@gmail.com>
2018-12-03 12:42:32 -0500

    Day 1 Solutions

---
+++ AoC2018_day1.py
@@ -0,0 +1,41 @@
+freqs = []
+subfreq = []
+current_freq = 0
+i = 0
+
+with open("C:\\Users\\jrecord\\Desktop\\AoC2018_day1.txt") as f:
+    line = f.readline()
+    line = line.strip("+")
+    cnt = 1
+    while line:
+        freqs.append(int(line))
+        line = f.readline()
+        line = line.strip("+")
+        cnt += 1
+
+print(sum(freqs))
+
+while True:
+    # print("i is {}".format(i))
+    # print(len(subfreq))
+    # print(subfreq)
+
+    res_freq = current_freq + freqs[i]
+
+    if res_freq in subfreq:
+        print(res_freq)
+        break
+
+    subfreq.append(res_freq)
+
+    current_freq = res_freq
+    
+    i += 1
+    if i == len(freqs):
+        i = 0
+
+    
+
+    
+    
+
